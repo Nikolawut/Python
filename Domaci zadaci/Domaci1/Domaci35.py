7ada294bf17a367755ca116bc1c850aa554d589a
Nikola <106932301+Nikolafuku@users.noreply.github.com>
2024-03-06 19:53:03 +0100

    e
    e

--- Domaci zadaci/Domaci1/Domaci35.py
+++ Domaci zadaci/Domaci1/Domaci35.py
@@ -3,4 +3,4 @@ while f != True:
     t = str(input("Unesite petocifreni broj:"))
     if len(t) == 5:
         f = True
-print(f"Sprat je: {int(t[2])+int(t[-1])}")
+print(f"Sprat je: {int(t[len(t)//2])+int(t[-1])}")
